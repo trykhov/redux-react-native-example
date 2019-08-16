1f9b587ad64fc3b51d9d555a0598b4630d87c9e3
Try <khovct@gmail.com>
2019-08-15 17:37:48 -0700

    designed reducer

--- App.js
+++ App.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux' // allows React to interact with redux
+
+
+const initState = {
+  myCounter: 0
+};
+
+const reducer = (state = initState, action) => {
+  switch(action.type) {
+    case "INC_COUNTER":
+      return { myCounter: state.myCounter + 1};
+    case "DEC_COUNTER":
+      return state.myCounter >= 1 ? { myCounter: state.myCounter - 1} : state;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 export default function App() {
   return (
